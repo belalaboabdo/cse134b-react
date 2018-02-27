c68bff0e7e6c15900da613450604d2e551456489
belalaboabdo <bilalaboabdo@gmail.com>
2018-02-27 09:36:29 -0800

    belal-homepage-and-header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,45 @@
 import React, {PropTypes} from 'react';
-import { Link, IndexLink } from 'react-router';
+import { Link, IndexLink} from 'react-router';
 import LoadingDots from './LoadingDots';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import { IndexLinkContainer } from 'react-router-bootstrap';
 
 const Header = ({loading}) => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {loading && <LoadingDots interval={100} dots={20}/>}
-    </nav>
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#brand">Gameflix</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">
+            <Link to="/about"> Community </Link>
+          </NavItem>
+       
+          <NavItem eventKey={2} href="#">
+            Profile
+          </NavItem>
+
+          <NavItem eventKey={2} href="#">
+            Messages
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Settings
+          </NavItem>
+          <NavItem eventKey={2} href="#">
+            Log out
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
+
 
 Header.propTypes = {
   loading: PropTypes.bool.isRequired
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,16 +1,59 @@
-import React from 'react';
-import {Link} from 'react-router';
+import React from "react";
+import { Link } from "react-router";
+import {Carousel} from "react-bootstrap";
 
 class HomePage extends React.Component {
-  render() {
-    return (
-      <div className="jumbotron">
-        <h1>Pluralsight Administration</h1>
-        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Carousel>
+				<Carousel.Item>
+					<img
+						width={900}
+						height={500}
+						alt="900x500"
+						src="/900x500.jpg"
+					/>
+					<Carousel.Caption>
+						<h3>First slide label</h3>
+						<p>
+							Nulla vitae elit libero, a pharetra augue mollis
+							interdum.
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						width={900}
+						height={500}
+						alt="900x500"
+						src="/900x500.jpg"
+					/>
+					<Carousel.Caption>
+						<h3>Second slide label</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit.
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						width={900}
+						height={500}
+						alt="900x500"
+						src="/900x500.jpg"
+					/>
+					<Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>
+							Praesent commodo cursus magna, vel scelerisque nisl
+							consectetur.
+						</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+		);
+	}
 }
 
 export default HomePage;
